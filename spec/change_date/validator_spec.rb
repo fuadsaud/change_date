21b7b832908c96ec921183840ece0a7ecb0cc520
Fuad Saud <fuadksd@gmail.com>
2014-04-10 02:31:21 -0300

    Add validation happy path spec

--- spec/change_date/validator_spec.rb
+++ spec/change_date/validator_spec.rb
@@ -1,19 +1,42 @@
 require_relative '../../lib/change_date/validator'
 
 describe ChangeDate::Validator do
- describe '#validate!' do
-   context 'invalid operator' do
-     subject { described_class.new }
+  describe '#validate!' do
+    subject { described_class.new }
 
-     let(:date) { '01/03/2010 23:00' }
-     let(:operator) { '*' }
-     let(:value) { 45 }
+    let(:date) { '01/03/2010 23:00' }
+    let(:value) { 45 }
 
-     it 'fails the validation' do
-       expect {
-         subject.validate!(date: date, operator: operator, value: value)
-       }.to raise_error ArgumentError
-     end
-   end
- end
+    context 'valid operator' do
+      context '+' do
+        let(:operator) { '+' }
+
+        it 'does not fail' do
+          expect {
+            subject.validate!(date: date, operator: operator, value: value)
+          }.to_not raise_error
+        end
+      end
+
+      context '-' do
+        let(:operator) { '+' }
+
+        it 'does not fail' do
+          expect {
+            subject.validate!(date: date, operator: operator, value: value)
+          }.to_not raise_error
+        end
+      end
+    end
+
+    context 'invalid operator' do
+      let(:operator) { '*' }
+
+      it 'fails the validation' do
+        expect {
+          subject.validate!(date: date, operator: operator, value: value)
+        }.to raise_error ArgumentError
+      end
+    end
+  end
 end
